25524baf88f14f1a8ef9b117b7747595055a6b66
Manelle_G;` <manelle.gueriouz@gmail.com>
2018-07-06 10:56:19 +0200

    few changes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,129 +1,124 @@
-
 console.log(google.maps);
 
-const mapDiv= document.querySelector(".my-google-map");
+const mapDiv = document.querySelector(".my-google-map");
 const locInput = document.querySelector(".location-input");
 //const latInput =document.(".lat-input");
-const longInput =document.querySelector(".long-input");
+const longInput = document.querySelector(".long-input");
 
-
+console.log(locInput);
 let map;
 
-if (mapDiv){
+if (mapDiv) {
   startMap();
 }
 
-if (locInput){
+if (locInput) {
   startPlaceAutocomplete();
 }
 
-function startPlaceAutocomplete(){
-  const autocomplete =
-  new google.maps.places.Autocomplete(locInput);
+function startPlaceAutocomplete() {
+  const autocomplete = new google.maps.places.Autocomplete(locInput);
 
-  autocomplete.addListener("place_changed", ()=>{
-    const place= autocomplete.getPlace();
-    console.log(place)
+  autocomplete.addListener("place_changed", () => {
+    const place = autocomplete.getPlace();
+    console.log(place);
 
-    const loc =place.geometry.location;
-    latInput.value=loc.lat();
-    longInput.value= loc.lng();
-  })
+    const loc = place.geometry.location;
+    latInput.value = loc.lat();
+    longInput.value = loc.lng();
+  });
 }
 
-
-
-function startMap(potluckItem){
-//Draw a Map
-map =
-new google.maps.Map(mapDiv, {
-    zoom:10,
-    center:{
-        lat:48.8589507,
-        lng:2.3470201
+function startMap(potluckItem) {
+  //Draw a Map
+  map = new google.maps.Map(mapDiv, {
+    zoom: 10,
+    center: {
+      lat: 48.8589507,
+      lng: 2.3470201
     },
     styles: [
-      {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-      {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
       {
-        featureType: 'administrative.locality',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }]
       },
       {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }]
       },
       {
-        featureType: 'poi.park',
-        elementType: 'geometry',
-        stylers: [{color: '#263c3f'}]
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#263c3f" }]
       },
       {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#6b9a76'}]
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }]
       },
       {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{color: '#38414e'}]
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#38414e" }]
       },
       {
-        featureType: 'road',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#212a37'}]
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }]
       },
       {
-        featureType: 'road',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#9ca5b3'}]
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#9ca5b3" }]
       },
       {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{color: '#746855'}]
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#746855" }]
       },
       {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#1f2835'}]
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }]
       },
       {
-        featureType: 'road.highway',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#f3d19c'}]
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }]
       },
       {
-        featureType: 'transit',
-        elementType: 'geometry',
-        stylers: [{color: '#2f3948'}]
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#2f3948" }]
       },
       {
-        featureType: 'transit.station',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }]
       },
       {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{color: '#17263c'}]
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#17263c" }]
       },
       {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#515c6d'}]
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#515c6d" }]
       },
       {
-        featureType: 'water',
-        elementType: 'labels.text.stroke',
-        stylers: [{color: '#17263c'}]
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#17263c" }]
       }
     ]
   });
-  
+
   // new google.maps.Marker({
   //   map,
   //   position:{
@@ -133,18 +128,17 @@ new google.maps.Map(mapDiv, {
   //   tittle:"it's happening here",
   //   animation:google.maps.Animation.DROP
   // })
-  
 }
 
-  // new google.maps.Marker({
-    //   map,
-    //   position:{
-      //     lat:18.3894,
-      //     lng:-66.1305
-//   },
-//   tittle:"San Juan, Puerto Rico",
-//   animation:google.maps.Animation.DROP
-// })
+new google.maps.Marker({
+  map,
+  position: {
+    lat: 18.3894,
+    lng: -66.1305
+  },
+  tittle: "San Juan, Puerto Rico",
+  animation: google.maps.Animation.DROP
+});
 
 // new google.maps.Marker({
 //   map,
@@ -160,10 +154,8 @@ new google.maps.Map(mapDiv, {
 // navigator.geolocation.getCurrentPosition((result)=>{
 //   const { latitude, longitude} = result.coords;
 
-
-  //make the map show the user's location
-  // map.setCenter({lat: latitude, lng: longitude});
-
+//make the map show the user's location
+// map.setCenter({lat: latitude, lng: longitude});
 
 //draw a marker where the user is
 // });
